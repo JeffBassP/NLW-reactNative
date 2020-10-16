3c9f1e61c930378626e5ff2ed9436b94185e279a
Jeferson <jeferson_jfs@hotmail.com>
2020-10-16 19:19:20 -0300

    finalizando o app mobile

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,66 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
 import { Feather } from '@expo/vector-icons';
 
 import mapMarker from '../images/map-marker.png';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../services/api'
+
+interface Orphanage {
+  id: number,
+  name: string,
+  latitude: number,
+  longitude: number,
+}
 
 export default function OrphanagesMap() {
   const navigation = useNavigation();
-  
-  function handleNavigateToOrphanageDetail() {
-    navigation.navigate('OrphanageDetails');
-  } 
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useFocusEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    })
+  })
+
+  function handleNavigateToOrphanageDetails(id: number) {
+    navigation.navigate('OrphanageDetails', { id });
+  }
+  function handlenavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
+  }
 
   return (
     <View style={styles.container}>
-    <MapView
-    provider={PROVIDER_GOOGLE}
-    style={styles.map}
-    initialRegion={{
-      latitude: -23.5953941,
-      longitude: -46.8543534,
-      latitudeDelta: 0.008,
-      longitudeDelta: 0.008
-    }}>
-    <Marker
-      icon={mapMarker}
-      coordinate={{
-        latitude: -23.5953941,
-        longitude: -46.8543534,
-      }}
-      calloutAnchor={{
-        x: 2.7,
-        y: 0.85,
-      }}
-    >
-      <Callout
-        tooltip
-        onPress={handleNavigateToOrphanageDetail}
-      >
-        <View style={styles.calloutContainer}>
-          <Text style={styles.calloutText}>Lar das meninas</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -23.5953941,
+          longitude: -46.8543534,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}>
+        {
+          orphanages.map(orphanage => {
 
-        </View>
+            return (
+              <Marker
+                key={orphanage.id}
+                icon={mapMarker}
+                coordinate={{
+                  latitude: orphanage.latitude,
+                  longitude: orphanage.longitude,
+                }}
+                calloutAnchor={{
+                  x: 2.7,
+                  y: 0.85,
+                }}
+              >
+                <Callout
+                  tooltip
+                  onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}
+                >
+                  <View style={styles.calloutContainer}>
+                    <Text style={styles.calloutText}>{orphanage.name}</Text>
 
-      </Callout>
-    </Marker>
+                  </View>
 
-  </MapView>
-  <View style={styles.footer}>
-    <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
-    <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
-      <Feather name="plus" size={20} color="#fff" />
-    </TouchableOpacity>
-  </View>
-</View>
+                </Callout>
+              </Marker>
+            )
+          })
+        }
+
+      </MapView>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
+        <RectButton style={styles.createOrphanageButton} onPress={handlenavigateToCreateOrphanage}>
+          <Feather name="plus" size={20} color="#fff" />
+        </RectButton>
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
   },
   map: {
     width: Dimensions.get('window').width,
